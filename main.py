582ce8cca3fe06a2546838cc38d1d825f54ce6e9
Marcela Souza <marcelasouza2804@gmail.com>
2022-04-21 16:14:56 -0300

    Atualizando a calculadora para versão 2.0

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 
 def somar(valor1, valor2):
-    resultado = valor1 + valor2
-    print('O resultado da soma é {}'.format(resultado))
+    return valor1 + valor2
 
 
 def subtracao(valor1, valor2):
-    resultado = valor1 - valor2
-    print('O resultado da subtração é {}'.format(resultado))
+    return valor1 - valor2
 
 
 def divisao(valor1, valor2):
-    resultado = valor1 / valor2
-    print('O resultado da divisão é {}'.format(resultado))
+    return valor1 / valor2
 
 
 def multiplicacao(valor1, valor2):
-    resultado = valor1 * valor2
-    print('O resultado da multiplicação é {}'.format(resultado))
+    return valor1 * valor2
 
 
 texto_pergunta = 'Digite o {} valor: '
@@ -25,13 +21,16 @@ texto_pergunta = 'Digite o {} valor: '
 primeiro_valor = float(input(texto_pergunta.format('primeiro')))
 segundo_valor = float(input(texto_pergunta.format('segundo')))
 
-somar(primeiro_valor, segundo_valor)
-subtracao(primeiro_valor, segundo_valor)
-divisao(primeiro_valor, segundo_valor)
-multiplicacao(primeiro_valor, segundo_valor)
+resultado_soma = somar(primeiro_valor, segundo_valor)
+resultado_subtracao = subtracao(primeiro_valor, segundo_valor)
+resultado_divisao= divisao(primeiro_valor, segundo_valor)
+resultado_multiplicacao= multiplicacao(primeiro_valor, segundo_valor)
 
+print(30 * '-')
 
+texto_resultado = 'O resultado da {} é: {} '
 
-
-
-
+print (texto_resultado.format('soma', resultado_soma))
+print (texto_resultado.format('subtração', resultado_subtracao))
+print (texto_resultado.format('divisão', resultado_divisao))
+print (texto_resultado.format('multiplicação', resultado_multiplicacao))
