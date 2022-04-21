9cd9c80ed5427c25d04b45579cf523850b21cb60
Marcela Souza <marcelasouza2804@gmail.com>
2022-04-21 20:05:29 -0300

    Atualizando a calculadora para versão 2.1

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ def divisao(valor1, valor2):
 def multiplicacao(valor1, valor2):
     return valor1 * valor2
 
+def definir_opcao(opcao):
+    if opcao == 1:
+        return somar(primeiro_valor, segundo_valor)
+    elif opcao == 2:
+        return subtracao(primeiro_valor, segundo_valor)
+    elif opcao == 3:
+        return divisao(primeiro_valor, segundo_valor)
+    elif opcao == 4:
+        return multiplicacao(primeiro_valor, segundo_valor)
+    else:
+        print('Opcção inexistente')
+
 
 texto_pergunta = 'Digite o {} valor: '
 
@@ -21,16 +33,13 @@ texto_pergunta = 'Digite o {} valor: '
 primeiro_valor = float(input(texto_pergunta.format('primeiro')))
 segundo_valor = float(input(texto_pergunta.format('segundo')))
 
-resultado_soma = somar(primeiro_valor, segundo_valor)
-resultado_subtracao = subtracao(primeiro_valor, segundo_valor)
-resultado_divisao= divisao(primeiro_valor, segundo_valor)
-resultado_multiplicacao= multiplicacao(primeiro_valor, segundo_valor)
 
-print(30 * '-')
+print('[1] Soma')
+print('[2] Subtração')
+print('[3] Divisão')
+print('[4] Multiplicação')
+opcao = int(input('Escolha uma opção: '))
 
-texto_resultado = 'O resultado da {} é: {} '
+resultado = definir_opcao(opcao)
 
-print (texto_resultado.format('soma', resultado_soma))
-print (texto_resultado.format('subtração', resultado_subtracao))
-print (texto_resultado.format('divisão', resultado_divisao))
-print (texto_resultado.format('multiplicação', resultado_multiplicacao))
+print('Resultado: {}'.format(resultado))
